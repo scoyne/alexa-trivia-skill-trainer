30af0555ff558ff55a39674707d5b704a404f479
scoyne <scoyne@bw.edu>
2018-08-05 14:09:43 -0400

    alexa skill trainer - irish dance trivia

---
+++ lambda/custom/questions.js
@@ -0,0 +1,279 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_EN_US: [
+    {
+      'What KIND of dance is Hurry the Jug? ': [
+        'Trebel Jig',
+        'Hornppe',
+        'Reel',
+        'Jig',
+      ],
+    },
+    {
+      'What is the recommended speed of The Blackbird? ': [
+        'one hundred thirth-eight',
+        'sixty-six',
+        'eighty',
+        'seventy-four',
+      ],
+    },
+    {
+      'What kind of dance is The Siege of Ennis? ': [
+        'Ceili',
+        'Hornpipe',
+        'Trebel Jig',
+        'Drama',
+      ],
+    },
+    {
+      'How many judges decide who wins in a Preliminary Championship competition? ': [
+        'Three',
+        'One',
+        'Four',
+        'Five',
+      ],
+    },
+    {
+      'Which of the following is a soft-shoe dance? ': [
+        'Reel',
+        'Treble Jig',
+        'St. Patricks Day dance',
+        'Hornpipe',
+      ],
+    },
+    {
+      'Each year there is a regional competition. What are these regional competitions called? ': [
+        'Oireachtas',
+        'Inishfrees',
+        'Ceili',
+        'Nationals',
+        'Feis',
+      ],
+    },
+    {
+      'What is a local dance competition called? ': [
+        'Feis',
+        'National',
+        'Ceili',
+        'Irish Kitchen Party',
+        'Hooley',
+      ],
+    },
+    {
+      'What level of competition would one advance to after Novice? ': [
+        'Prizewinner',
+        'Preliminary Championship',
+        'Open Championship',
+        'Advanced Beginner',
+        'Lord of the Dance',
+      ],
+    },
+    {
+      'Most girls wear wigs during competition.  Why do they do this? ': [
+        'They do not all have the naturlly curly hair that is a tradition to wear',
+        'Irish dance teacher demand that everyone wear a blonde wig',
+        'They are not allowed to show their real hair',
+        'It is required to wear a wig',
+        'Some Irish dancers are bald and the judges would find that to be a distraction'
+      ],
+    },
+    {
+      'How many times must a Preliminary Championship competitor win first place before advancing to Open Championship? ': [
+        'Two',
+        'One',
+        'Five',
+        'Three',
+      ],
+    },
+    {
+      'Which of these dances is usually a beginner dance and is not use in the championship level? ': [
+        'Light Jig',
+        'Hornpipe',
+        'Slip Jig',
+        'Reel',
+      ],
+    },
+    {
+      'What color are Irish Dancing soft shoes? ': [
+        'black',
+        'white',
+        'red',
+        'brown',
+        'they can be any color',
+      ],
+    },
+    {
+      'What color are Irish Dancing hard shoes? ': [
+        'black',
+        'white',
+        'red',
+        'navy blue',
+        'they can be any color',
+      ],
+    },
+    {
+      'Usualy girls and boys dance in solo competition.  Which of the follow is usually a dance with multiple people? ': [
+        'Ceili',
+        'Set Piece',
+        'Hornpipe',
+        'Jig',
+      ],
+    },
+    {
+      'When did Riverdance first premier as a full show? ': [
+        'nineteen ninty-five',
+        'nineteen ninty-six',
+        'nineteen ninety-four',
+        'nineteen ninety-three',
+        'nineteen eighty-nine'
+      ],
+    },
+    {
+      'Who was the first leading lady in Riverdance? ': [
+        'Jean Butler',
+        'Bernadete Flynn',
+        'Eileen Martin',
+        'Tara Barry',
+      ],
+    },
+    {
+      'Who was the first leading man in Riverdance? ': [
+        'Michael Flately',
+        'Pat Roddy',
+        'Michael Patrick Gallagher',
+        'Colin Dunne',
+      ],
+    },
+    {
+      'The singer Enya sang the soundtrack for Riverdance? ': [
+        'false',
+        'true',
+      ],
+    },
+    {
+      'What choral group was in the original Riverdance production? ': [
+        'Anuna',
+        'Spice Girls',
+        'N Sync',
+        'Backstreet Boys',
+      ],
+    },
+    {
+      'Which song title is NOT part of the Riverdance soundtrack? ': [
+        'Breakout',
+        'Reel Around the Sun',
+        'Heartland',
+        'Shivna',
+      ],
+    },
+    {
+      'Where did Riverdance first originate? ': [
+        'Dublin, Ireland',
+        'New York City',
+        'London, England ',
+        'Sydney, Australia',
+      ],
+    },
+    {
+      'What is NOT a term used to describe Irish Dance shoes used to perform a Slip Jig? ': [
+        'Jig Shoes',
+        'Pumps',
+        'Light Shoes',
+        'Ghillies',
+      ],
+    },
+    {
+      'What is NOT a term used to describe Irish Dance shoes used to perform a Hornpipe? ': [
+        'Clogs',
+        'Heavy Shoes',
+        'Jig Shoes',
+        'Hard Shoes',
+      ],
+    },
+    {
+      'Which is NOT a Traditional Set Dance? ': [
+        'Planxty Drury',
+        'Garden of Dasies',
+        'Saint Patricks Day',
+        'The Blackbird',
+      ],
+    },
+    {
+      'Which of the following is not a type of Irish Dance? ': [
+        'Square Dance',
+        'Figure Dance',
+        'Ceili Dance',
+        'Set Dance',
+      ],
+    },
+    {
+      'What is the dress called that is worn by an Irish Dancer at the highest level of competition? ': [
+        'Solo Dress',
+        'Gavinesse Dress',
+        'One-off Dress',
+        'School Dress',
+      ],
+    },
+    {
+      'Which ancient manuscript is the source material for many Celtic designs used on Irish Dance dresses? ': [
+        'Book of Kells',
+        'Book of Kent',
+        'Book of Armagh',
+        'Book of Kerry',
+      ],
+    },
+    {
+      'In Irish Dancing, the placement of the feet is very important. How are you supposed to place your feet while dancing? ': [
+        'With your feet turned out, one in front of the other',
+        'With your feet turned out, both beside each other',
+        'With your feet turn in, one behind the other',
+        'With our feet turned in, both beside each other',
+      ],
+    },
+    {
+      'In Irish Dancing, how are you to supposed to hold your hands while dancing in a solo competition? ': [
+        'Hands down by your sides',
+        'Hands on your hips',
+        'Left hand by your side, right hand on hip',
+        'Right hand by side, left hand on hip',
+      ],
+    },
+    {
+      'What other popular type of dance has been influnced by Irish Dancing? ': [
+        'Tap Dance',
+        'Latin Dance',
+        'Hip Hop',
+        'Ballet',
+      ],
+    },
+    {
+      'What kind of dance is The Siege of Ennis? ': [
+        'Ceili',
+        'Hornpipe',
+        'Drama',
+        'Treble Jig',
+      ],
+    },
+    {
+      'In the United States, what level of Irish Dance Competitor performs a set dance as part of their competition? ': [
+        'Open Championship',
+        'Preliminary Championship',
+        'Prize winner',
+        'Novise',
+      ],
+    },
+    {
+      'Which of the following set dances is NOT considered a traditional set? ': [
+        'The Story teller',
+        'Job of Journey work',
+        'The Blackbird',
+        'Saint Patricks Day',
+      ],
+    },
+  ], 
+};
